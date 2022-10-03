d27d794bead4a5519ed2ceb3d448739f7bd7fb9d
Rose Khaldeeva <khalrose@mail.ru>
2022-10-03 19:59:07 +0600

    Work with WelcomeVC

--- TwoScreensApp/WelcomeViewController.swift
+++ TwoScreensApp/WelcomeViewController.swift
@@ -48,30 +48,4 @@ extension UIView {
 }
     
 
-    /*override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let color1 = UIColor(red: 153 / 255, green: 26 / 255, blue: 61 / 255, alpha: 1.0).cgColor
-        let color2 = UIColor(red: 243/255, green: 155 / 255, blue: 51 / 255, alpha: 1.0).cgColor
-        
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = self.view.bounds
-        gradientLayer.colors = [color1, color2]
-        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
-        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
-        self.view.layer.insertSublayer(gradientLayer, at: 0)
-        }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
-*/
+  
